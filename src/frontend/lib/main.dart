6f9b0d034b783e6d121befba08c69709d64c0318
kaznak <kaznak.at.work@gmail.com>
2021-10-01 20:11:23 +0900

    feat(frontend): lib/main.dart handle AuthService

--- src/frontend/lib/main.dart
+++ src/frontend/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'login_page.dart';
 import 'sign_up_page.dart';
+import 'auth_service.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,19 +14,48 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  final _authService = AuthService();
+
+  @override
+  void initState() {
+    super.initState();
+    _authService.showLogin();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Photo Gallery App',
       theme: ThemeData(visualDensity: VisualDensity.adaptivePlatformDensity),
       // 2
-      home: Navigator(
-        pages: [
-          MaterialPage(child: LoginPage()),
-          MaterialPage(child: SignUpPage())
-        ],
-        onPopPage: (route, result) => route.didPop(result),
-      ),
+      home: StreamBuilder<AuthState>(
+          // 2
+          stream: _authService.authStateController.stream,
+          builder: (context, snapshot) {
+            // 3
+            if (snapshot.hasData) {
+              return Navigator(
+                pages: [
+                  // 4
+                  // Show Login Page
+                  if (snapshot.data!.authFlowStatus == AuthFlowStatus.login)
+                    MaterialPage(child: LoginPage()),
+
+                  // 5
+                  // Show Sign Up Page
+                  if (snapshot.data!.authFlowStatus == AuthFlowStatus.signUp)
+                    MaterialPage(child: SignUpPage())
+                ],
+                onPopPage: (route, result) => route.didPop(result),
+              );
+            } else {
+              // 6
+              return Container(
+                alignment: Alignment.center,
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
     );
   }
 }
