e392bc57b74529de72c6f5b0f3f8bc560f84128b
kaznak <kaznak.at.work@gmail.com>
2021-10-01 21:15:04 +0900

    feat(frontend:auth): lib/auth_service.dart uses auth credentials

--- src/frontend/lib/auth_service.dart
+++ src/frontend/lib/auth_service.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'auth_credentials.dart';
 
 // 1
 enum AuthFlowStatus { login, signUp, verification, session }
@@ -24,6 +25,18 @@ class AuthService {
   // 6
   void showLogin() {
     final state = AuthState(authFlowStatus: AuthFlowStatus.login);
+    authStateController.add(state);
+  }
+
+  // 1
+  void loginWithCredentials(AuthCredentials credentials) {
+    final state = AuthState(authFlowStatus: AuthFlowStatus.session);
+    authStateController.add(state);
+  }
+
+  // 2
+  void signUpWithCredentials(SignUpCredentials credentials) {
+    final state = AuthState(authFlowStatus: AuthFlowStatus.verification);
     authStateController.add(state);
   }
 }
